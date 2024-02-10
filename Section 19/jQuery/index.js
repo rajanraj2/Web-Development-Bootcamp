f7cf24689251bcaa6a0b82a63024c495ae6c83d0
rajan raj <rans2203@gmail.com>
2024-02-10 11:30:48 +0530

    Files added

---
+++ Section 19/jQuery/index.js
@@ -0,0 +1,28 @@
+
+// $("h1").click(function(){
+//     $("h1").css("color", "purple")
+// });
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "purple");
+// })
+
+// $("button").click(function(){
+//     $("h1").slideToggle();
+// });
+
+$("button").click(function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// });
+
+// $("h1").text("Bye");
+// $("h1").addClass("big-title margin-50");
+
+// $("button");
+
+// // console.log($("img").attr("src"));
+// $("a").attr("href", "https://yahoo.com");
