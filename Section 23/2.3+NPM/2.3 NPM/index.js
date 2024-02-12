3865b3ef6b7f74cd9daa8930c0f1951d0fba9298
rajan raj <rans2203@gmail.com>
2024-02-13 00:35:57 +0530

    Files added

---
+++ Section 23/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname"
+
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+import superheroes from "superheroes";
+
+var superheroName = superheroes.random();
+
+console.log(`I am ${superheroName}!`)
